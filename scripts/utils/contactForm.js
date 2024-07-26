674c1254edb69454164cad3352aaadc819384abd
samaki8 <samira08@gmail.com>
2024-07-26 12:26:35 +0100

    Resize jpg photographers +  supp obj-fit:covert
    Resize les jpg  height/width 200px + supp object-fit: cover

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,3 +1,13 @@
+// Global DOM variables
+const modal = document.getElementById('contact_modal');
+const openModalBtn = document.querySelector('.contact_button');
+const closeModalBtn = document.querySelector('.modal-close-btn');
+
+// Event listeners
+openModalBtn.addEventListener('click', displayModal);
+closeModalBtn.addEventListener('click', closeModal);
+
+
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
