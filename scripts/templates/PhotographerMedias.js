33611375bca7aad7b00539d80d4c5b1ee0c1b394
samaki8 <samira08@gmail.com>
2024-08-07 13:01:13 +0100

    changement de code photographermedia

--- scripts/templates/PhotographerMedias.js
+++ scripts/templates/PhotographerMedias.js
@@ -1,4 +1,4 @@
-
+/*
 export default class PhotographerMedias {
     constructor(photographer, medias) {
         this.photographer = photographer;
@@ -52,3 +52,64 @@ export default class PhotographerMedias {
         return content;
     };
 };
+*/
+
+export default class PhotographerMedias {
+    constructor(photographer, medias) {
+        this.photographer = photographer;
+        this.medias = medias;
+    }
+
+    createPhotographerMedias() {
+        const profilePageContent = document.querySelector(".main_content_medias");
+        const content = `
+            <section class="gallery">
+                ${this.medias.map(media => this.createMediaCard(media)).join("")}
+            </section>
+            <aside>
+                <p class="photographer_Likes">
+                    <span class="photographer_likes_count"></span>
+                    <span class="fas fa-heart" aria-hidden="true"></span>
+                </p>
+                <span>${this.photographer.price}€ / jour</span>
+            </aside>
+        `;
+
+        profilePageContent.innerHTML = content;
+        return content;
+    }
+
+    createMediaCard(media) {
+        const mediaContent = media.image
+            ? this.createImage(media)
+            : this.createVideo(media);
+
+        return `
+            <article class="gallery_card">
+                <a href="#" data-media=${media.id} role="link">
+                    <figure>${mediaContent}</figure>
+                </a>
+                <figcaption>
+                    <h2>${media.title}</h2>
+                    <div role="group" aria-label="Like button and number of likes">
+                        <span class="nbLike">${media.likes}</span>
+                        <button class="btn_like" type="button" aria-label="Like" data-id="${media.id}">
+                            <span class="fas fa-heart" aria-hidden="true"></span>
+                        </button>
+                    </div>
+                </figcaption>
+            </article>
+        `;
+    }
+
+    createImage(media) {
+        return `<img class="gallery_thumbnail" src="./assets/images/${this.photographer.name}/${media.image}" alt="${media.title}">`;
+    }
+
+    createVideo(media) {
+        return `
+            <video class="gallery_thumbnail" aria-label="${media.title}">
+                <source src="./assets/images/${this.photographer.name}/${media.video}" type="video/mp4">
+            </video>`;
+    }
+}
